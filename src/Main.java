d5d74f75d3c8e7d84584047e107fa31e7d106669
Reginald Long <reggylong@gmail.com>
2016-06-19 20:48:13 +0100

    added reading json from carwl

--- src/Main.java
+++ src/Main.java
@@ -11,54 +11,77 @@ import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations;
 import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
 import edu.stanford.nlp.trees.*;
 import edu.stanford.nlp.util.*;
+import javax.json.*;
 
 public class Main {
 
+  private static List<Annotation> annotations = new ArrayList<>();
+  private static StanfordCoreNLP pipeline;
+  private static final String input = "release/crawl";
+
   public static void main(String[] args) throws IOException {
-    // set up optional output files
-    PrintWriter out;
-    if (args.length > 1) {
-      out = new PrintWriter(args[1]);
-    } else {
-      out = new PrintWriter(System.out);
-    }
-    PrintWriter xmlOut = null;
-    xmlOut = new PrintWriter("output");
 
-    // Create a CoreNLP pipeline. To build the default pipeline, you can just use:
-    //   StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-    // Here's a more complex setup example:
-    //   Properties props = new Properties();
-    //   props.put("annotators", "tokenize, ssplit, pos, lemma, ner, depparse");
-    //   props.put("ner.model", "edu/stanford/nlp/models/ner/english.all.3class.distsim.crf.ser.gz");
-    //   props.put("ner.applyNumericClassifiers", "false");
-    //   StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+    //initPipeline();
+    loadUtterances(input);
+    System.out.println("Finished loading input:" + input);
+    System.out.println("Processing annotations...");
+    processAnnotations();
+  }
 
-    // Add in sentiment
+  private static void initPipeline() {
     Properties props = new Properties();
-    props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref, sentiment");
+    props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
+    pipeline = new StanfordCoreNLP(props);
+  }
 
-    StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+  private static void loadUtterances(String filename) {
 
+    BufferedReader in = null;
+    try {
+      in = new BufferedReader(new FileReader(filename));
+    } catch (FileNotFoundException e) {
+      System.err.println(e.getMessage());
+      System.exit(1);
+    }
+
+    for (int i = 0; i < 10; i++) {
+      String line = null;
+      try {
+        line = in.readLine();
+      } catch (IOException e) { 
+        System.err.println(e.getMessage());
+        continue;
+      }
+      JsonReader reader = Json.createReader(new StringReader(line));
+      JsonObject object = reader.readObject();
+
+      String date = object.getJsonString("date").getString();
+      String title = object.getJsonString("title").getString();
+      String url = object.getJsonString("url").getString();
+      String text = object.getJsonString("text").getString();
+      int id = object.getInt("articleId");
+      System.out.println(id);
+      reader.close();
+    }
+    System.exit(0);
+  }
+
+  private static void processAnnotations() {
+    PrintWriter xmlOut = null;
+    try {
+      xmlOut = new PrintWriter("output");
+    } catch (Exception e) {}
     // Initialize an Annotation with some text to be annotated. The text is the argument to the constructor.
     Annotation annotation;
-    if (args.length > 0) {
-      annotation = new Annotation(IOUtils.slurpFileNoExceptions(args[0]));
-    } else {
-      annotation = new Annotation("Kosgi Santosh sent an email to Stanford University. He didn't get a reply.");
-    }
+    annotation = new Annotation("Kosgi Santosh sent an email to Stanford University. He didn't get a reply.");
 
     // run all the selected Annotators on this text
     pipeline.annotate(annotation);
 
-    // this prints out the results of sentence analysis to file(s) in good formats
-    pipeline.prettyPrint(annotation, out);
-    if (xmlOut != null) {
+    try {
       pipeline.xmlPrint(annotation, xmlOut);
-    }
-
-    IOUtils.closeIgnoringExceptions(out);
+    } catch (Exception e) {}
     IOUtils.closeIgnoringExceptions(xmlOut);
-  }
 
+  }
 }
