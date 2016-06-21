ee8b919a3328615a312e8011409e7c1f8bd12b07
Reginald Long <reggylong@gmail.com>
2016-06-21 02:52:18 -0700

    added worker structure

--- src/Main.java
+++ src/Main.java
@@ -19,18 +19,24 @@ import java.lang.*;
 public class Main {
 
   private static final String input = "release/crawl";
+  public static final String workerInput = "inputs";
+  public static final String workerOutput = "outputs";
   private static final String output = "annotations.xml";
-  private static BufferedReader in;
-  private static final int nWorkers = 28;
+  private static final int nWorkers = 16;
 
   public static void main(String[] args) throws IOException {
 
-    System.out.println("Determining size of dataset");
-    int count = countLines(input);
-    int remaining = count;
-    System.out.println("Distributing inputs");
-    distributeInputs(count);
+    if (!isCached()) {
+      System.out.println("Determining size of dataset");
+      int count = countLines(input);
+      int remaining = count;
+      System.out.println("Distributing inputs");
+      distributeInputs(count);
+    } else {
+      System.out.println("Using cached inputs...");
+    }
     System.out.println("Processing annotations...");
+    processAnnotations();
     System.out.println("Finished processing annotations");
   }
 
@@ -43,6 +49,27 @@ public class Main {
     printError(e);
     System.err.println("Exiting...");
     System.exit(1);
+  }
+
+  private static boolean isCached() {
+    File dir = new File(workerInput);
+    boolean cached = true;
+    if (!dir.exists()) return false;
+    for (int i = 0; i < nWorkers; i++) {
+      System.out.println("Checking " + workerInput + "/" + i + ".in");
+      File f = new File(workerInput + "/" + i + ".in");
+      if (!f.exists()) {
+        cached = false;
+        break;
+      }
+    }
+    if (new File(workerInput).listFiles().length != nWorkers) {
+      cached = false;
+    }
+    if (!cached) {
+      for (File file : dir.listFiles()) file.delete();
+    }
+    return cached;
   }
 
   private static void distributeInputs(int total) {
@@ -83,7 +110,7 @@ public class Main {
     } catch (IOException e) { printError(e); }
   }
 
-  private static Integer countLines(String filename) {
+  public static Integer countLines(String filename) {
     Process p = null;
     try {
       p = Runtime.getRuntime().exec("wc -l " + input);
@@ -101,26 +128,79 @@ public class Main {
     return null;
   }
 
-  private static StanfordCoreNLP initPipelines() {
+  /*
+  String date = object.getJsonString("date").getString();
+  String title = object.getJsonString("title").getString();
+  String url = object.getJsonString("url").getString();
+  String text = object.getJsonString("text").getString();
+  int id = object.getInt("articleId");*/
+
+  private static void processAnnotations() {
+    File dir = new File(workerOutput);
+    dir.mkdir();
+    
+    for (File file : dir.listFiles()) file.delete();
+
+    Thread threads[] = new Thread[nWorkers];
+    for (int i = 0; i < nWorkers; i++) {
+      Handler h = new Handler(i);
+      threads[i] = new Thread(h);
+      threads[i].start();
+    }
+    for (int i = 0; i < nWorkers; i++) {
+      try {
+      threads[i].join();
+      } catch (InterruptedException e) { Main.printError(e); }
+    }
+  }
+}
+
+class Handler implements Runnable {
+  private final int id;
+  private PrintWriter xmlOut;
+  private StanfordCoreNLP pipeline;
+  private BufferedReader in;
+  private int remainingLines;
+
+  Handler(int id) { 
+    this.id = id;
+    pipeline = initPipeline();
+    initOut();
+    initIn();
+  }
+
+  private static StanfordCoreNLP initPipeline() {
     Properties props = new Properties();
     props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner");
     return new StanfordCoreNLP(props);
   }
 
-  private static void openInput(String filename) {
+  private void initOut() {
+    String output = id + ".xml";
     try {
-      in = new BufferedReader(new FileReader(filename));
-    } catch (FileNotFoundException e) {
-      exit(e);
+      xmlOut = new PrintWriter(Main.workerOutput + "/" + output);
+    } catch (IOException e) {
+      System.err.println(e.getMessage());
+      System.exit(1);
     }
   }
 
-  private static JsonObject read() {
+  private void initIn() {
+    String input = id + ".in";
+    try {
+      in = new BufferedReader(new FileReader(Main.workerInput + "/" + input));
+    } catch (IOException e) {
+      Main.exit(e);
+    }
+    remainingLines = Main.countLines(Main.workerInput + "/" + input);
+  }
+
+  private JsonObject read() {
     String line = null;
     try {
       line = in.readLine();
     } catch (IOException e) {
-      printError(e);
+      Main.printError(e);
       return null;
     }
     if (line == null) return null;
@@ -130,83 +210,33 @@ public class Main {
     return object;
   }
 
-  /*
-  String date = object.getJsonString("date").getString();
-  String title = object.getJsonString("title").getString();
-  String url = object.getJsonString("url").getString();
-  String text = object.getJsonString("text").getString();
-  int id = object.getInt("articleId");*/
-
-  private static void processAnnotations() {
-    /*
-    JsonObject obj = null;
-    PrintWriter xmlOut = null;
-    try {
-      xmlOut = new PrintWriter(output);
-    } catch (Exception e) {
-      System.err.println(e.getMessage());
-      System.exit(1);
-    }
-
-    ReentrantLock l = new ReentrantLock();
-    int count = 0;
-
-    while ( (obj = read()) != null) {
-      count++;
-      pool.submit(new Handler(xmlOut, pipelines, obj, l));
-      if (count % 10000 == 0) System.out.println("Scheduled " + count + " utterances");
-    }
-    pool.shutdown();
-    try {
-      boolean done = pool.awaitTermination(1, TimeUnit.DAYS);
-    } catch (Exception e) {
-      System.err.println(e.getMessage());
-      System.err.println("pool.awaitTermination failed");
-    }
-    IOUtils.closeIgnoringExceptions(xmlOut); */
-  }
-}
-
-class Handler implements Runnable {
-  JsonObject obj;
-  ReentrantLock l;
-  PrintWriter xmlOut;
-  BlockingQueue<StanfordCoreNLP> pipelines;
-
-  Handler(PrintWriter xmlOut, BlockingQueue<StanfordCoreNLP> pipelines, 
-      JsonObject obj, ReentrantLock l) { 
-    this.pipelines = pipelines;
-    this.obj = obj; 
-    this.l = l; 
-    this.xmlOut = xmlOut; 
-  }
-
   public void run() { 
-    Annotation annotation = null;
-    StanfordCoreNLP pipeline = null;
-    try {
-      pipeline = pipelines.take();
-    } catch (InterruptedException e) {
-      Main.printError(e);
-      return;
+    JsonObject obj = null;
+    while ( (obj = read()) != null) {
+
+      Annotation annotation = null;
+      try {
+        annotation = new Annotation(obj.getJsonString("text").getString());
+      } catch (Exception e) {
+        Main.printError(e);
+        System.err.println(obj);
+      }
+      pipeline.annotate(annotation);
+      try {
+        xmlOut.println(obj.getInt("articleId"));
+        pipeline.xmlPrint(annotation, xmlOut);  
+        xmlOut.println();
+      } catch (Exception e) {
+        Main.printError(e);
+      }
+      remainingLines--;
+      if (remainingLines % 500 == 0) {
+        synchronized (System.out) {
+          System.out.println("Worker " + id + ": " 
+              + remainingLines + " examples left");
+        }
+      }
     }
-    try {
-      annotation = new Annotation(obj.getJsonString("text").getString());
-    } catch (Exception e) {
-      Main.printError(e);
-      System.err.println(obj);
-      return;
-    }
-    pipeline.annotate(annotation);
-    pipelines.add(pipeline);
-    l.lock();
-    try {
-      xmlOut.println(obj.getInt("articleId"));
-      pipeline.xmlPrint(annotation, xmlOut);  
-      xmlOut.println();
-    } catch (Exception e) {
-      System.err.println(e.getMessage());
-    }
-    l.unlock();
+    IOUtils.closeIgnoringExceptions(xmlOut);
   }
 }
