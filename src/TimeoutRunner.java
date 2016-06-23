740d3b1f7b364833475972c64b70962bf5b97d02
Reginald Long <reggylong@gmail.com>
2016-06-23 02:24:35 -0700

    added counter for failed runs

--- src/TimeoutRunner.java
+++ src/TimeoutRunner.java
@@ -30,11 +30,21 @@ class TimeoutRunner implements Runnable {
       System.err.println("TimeoutRunner: interrupted");
       endTask(future);
     } catch (ExecutionException e) {
+      int failed = Main.failed.incrementAndGet();
       System.err.println("Thread: " + runner + " threw an exception");
+      printFailed(failed);
       endTask(future);
     } catch (TimeoutException e) {
+      int failed = Main.failed.incrementAndGet();
       System.err.println("Thread: " + runner + " timed out");
+      printFailed(failed);
       endTask(future);
+    }
+  }
+
+  private void printFailed(int failed) {
+    if (failed % 10 == 0) {
+      System.out.println(failed + " number of executions have failed.");
     }
   }
 }
