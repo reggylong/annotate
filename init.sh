c28b7af3e6065ff0f2de291e9f542ff57962a0f8
Reginald Long <reggylong@gmail.com>
2016-06-23 01:51:11 -0700

    wrote new init.sh

--- init.sh
+++ init.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-#mkdir classes &&
-#mkdir resources &&
-#wget http://nlp.stanford.edu/software/stanford-corenlp-full-2015-12-09.zip &&
-#unzip stanford-corenlp-full-2015-12-09.zip &&
-#rm -rf stanford-corenlp-full-2015-12-09.zip
+mkdir classes &&
+mkdir resources &&
+wget http://nlp.stanford.edu/software/stanford-corenlp-full-2015-12-09.zip &&
+unzip stanford-corenlp-full-2015-12-09.zip &&
+rm -rf stanford-corenlp-full-2015-12-09.zip
 
-#for file in stanford-corenlp-full-2015-12-09/*.jar; do
-#  mv $file resources
-#done
+for file in stanford-corenlp-full-2015-12-09/*.jar; do
+  mv $file resources
+done
 
-#rm -rf stanford-corenlp-full-2015-12-09 &&
+rm -rf stanford-corenlp-full-2015-12-09 &&
 
-#cd resources &&
-#for file in *.jar; do
-#  jar xf $file
-#done
-#cd .. &&
+cd resources &&
+for file in *.jar; do
+  jar xf $file
+done
+wget http://nlp.stanford.edu/software/stanford-srparser-2014-10-23-models.jar
+cd .. 
 
+# website seems to be down
 wget http://www.cs.washington.edu/ai/clzhang/release.tar.gz &&
 tar zxvf release.tar.gz
